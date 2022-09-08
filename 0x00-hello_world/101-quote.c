1d14b1af70ae814dabff3617d5e1b58142a4ac35
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-08 12:33:50 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,12 @@
-#!/bin/bash 
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban 
- */
-
-#include <unistd.h>
+#include<unistd.h>
 
 /**
- *  main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19," 
- *                 followed by a new line, to the standard error.
- *
- *                 Return: Always 1.
- */
-int main(void) 
+* main - prints the standard error sandwiched around the  standard output
+*
+* Return: returns 1
+*/
+int main(void)
 {
-	write(2,
-	      "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
-	      59);
-
-              return (1);
-} 	      
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
