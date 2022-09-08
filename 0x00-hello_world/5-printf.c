4147fad8eb0261acc0b03ca55be808c889f96624
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-08 11:12:03 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,39 +1,10 @@
-#!/bin/bash
-/*
-
-      * File: 5-printf.c
-      *
-      *  * Auth: Brennan D Baraban
-      *
-      *   */
-
-
-
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Prints "with proper grammar, but the outcome is
- *
- *   *                a piece of art," followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+* main - main block 
+ * Return: 0
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
