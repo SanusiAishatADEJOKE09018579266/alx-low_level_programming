f2fcd194ac8f67c03abf6cf1a4435ff1b0fffd1a
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-08 08:28:06 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#!/bin/bash
 /*
- *
  *
  *  * File: 4-puts.c
  *
@@ -27,14 +25,13 @@
  *      */
 
 int main(void)
-	
+
 {
-	
+
 		puts("\"Programming is like building a multilingual puzzle");
-		
-		
-		
+
+
+
 			return (0);
-			
-}*  File: 4-puts.c
- *  Auth: 
+
+}
