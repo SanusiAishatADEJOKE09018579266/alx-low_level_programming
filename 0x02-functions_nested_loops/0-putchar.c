9c1c0579886b7e708b310583393b8d764aa9a309
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-14 13:30:21 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Print putchar 
- * Return: Always 0.
- */
+  * main - print a string putchar
+  * Return: 0 (Always success)
+  */
 int main(void)
 {
-	_putchar(' _ ');
-	_putchar('p);
+	putchar('');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
