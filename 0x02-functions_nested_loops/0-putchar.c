0d47ba93d643e40c5091240ab760fde058c1b22f
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-14 13:40:06 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
-#include "main.h"
+ #include "holberton.h"
 
 /**
-  * main - print a string putchar
-  * Return: 0 (Always success)
-  */
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
