46a77624bd741d93b4e1529720cdeb9b077b301b
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-16 05:33:01 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
-#nclude "main.h"
+#include "main.h"
+
 /**
- * main - prints positive or negative integer
- *Return: 0
+ * main -print positive or negative integer
+ * Return: Always 0
  */
 int main(void)
 {
 	int i;
-	i = 0;
-	positive_or_negative(i);
+	i = 0
+		positive_or_negative(i);
 	return (0);
 }
 
