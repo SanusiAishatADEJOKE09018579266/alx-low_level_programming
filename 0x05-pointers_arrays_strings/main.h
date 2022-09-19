19441b8870012603a8c2c5c8befcb2c94747c955
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-19 06:41:14 -0700

    task1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* void prototypes ()
-* not prototypes ()
-*/
+#include <stdlib.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
@@ -16,6 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
+int _atoi(char *s);
 
-#endif /*MAIN_H*/
+#endif
