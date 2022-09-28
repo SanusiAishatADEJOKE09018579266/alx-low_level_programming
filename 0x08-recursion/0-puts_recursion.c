6d0e3af53559130179f05c8affe69b314696cf7c
SanusiAishatADEJOKE09018579266 <sanusiaishat7@example.com>
2022-09-28 09:27:09 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _puts_recursion - Prints a string in reverse.
+ * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
